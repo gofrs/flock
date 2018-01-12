299035f6ea15c67837eddea9d38c4164ef0cf524
David E. Wheeler <david@justatheory.com>
2018-01-12 15:26:13 -0500

    Add support for shared locks
    Done by adding *RLock* variants of the Lock* methods and removing the file
    truncation. The two sets of methods rely the same code by dispatching to
    shared methods, only varying the lock type. The exception is RLocked(), which
    checks its own, separate boolean. All the lock operations share the same
    mutex, though, so any all operations can block each other. The docs have been
    updated to reflet that fact. Also fixed the Windows Unlock() docs to refer to
    UnlockFileEx() instead of syscall.LOCK_UN.

--- flock_test.go
+++ flock_test.go
@@ -50,6 +50,7 @@ func (t *TestSuite) TestNewFlock(c *C) {
 	c.Assert(f, Not(IsNil))
 	c.Check(f.Path(), Equals, t.path)
 	c.Check(f.Locked(), Equals, false)
+	c.Check(f.RLocked(), Equals, false)
 }
 
 func (t *TestSuite) TestFlock_Path(c *C) {
@@ -64,6 +65,12 @@ func (t *TestSuite) TestFlock_Locked(c *C) {
 	c.Check(locked, Equals, false)
 }
 
+func (t *TestSuite) TestFlock_RLocked(c *C) {
+	var locked bool
+	locked = t.flock.RLocked()
+	c.Check(locked, Equals, false)
+}
+
 func (t *TestSuite) TestFlock_String(c *C) {
 	var str string
 	str = t.flock.String()
@@ -72,6 +79,7 @@ func (t *TestSuite) TestFlock_String(c *C) {
 
 func (t *TestSuite) TestFlock_TryLock(c *C) {
 	c.Assert(t.flock.Locked(), Equals, false)
+	c.Assert(t.flock.RLocked(), Equals, false)
 
 	var locked bool
 	var err error
@@ -80,6 +88,7 @@ func (t *TestSuite) TestFlock_TryLock(c *C) {
 	c.Assert(err, IsNil)
 	c.Check(locked, Equals, true)
 	c.Check(t.flock.Locked(), Equals, true)
+	c.Check(t.flock.RLocked(), Equals, false)
 
 	locked, err = t.flock.TryLock()
 	c.Assert(err, IsNil)
@@ -88,6 +97,39 @@ func (t *TestSuite) TestFlock_TryLock(c *C) {
 	// make sure we just return false with no error in cases
 	// where we would have been blocked
 	locked, err = flock.NewFlock(t.path).TryLock()
+	c.Assert(err, IsNil)
+	c.Check(locked, Equals, false)
+}
+
+func (t *TestSuite) TestFlock_TryRLock(c *C) {
+	c.Assert(t.flock.Locked(), Equals, false)
+	c.Assert(t.flock.RLocked(), Equals, false)
+
+	var locked bool
+	var err error
+
+	locked, err = t.flock.TryRLock()
+	c.Assert(err, IsNil)
+	c.Check(locked, Equals, true)
+	c.Check(t.flock.Locked(), Equals, false)
+	c.Check(t.flock.RLocked(), Equals, true)
+
+	locked, err = t.flock.TryRLock()
+	c.Assert(err, IsNil)
+	c.Check(locked, Equals, true)
+
+	// shared lock should not block.
+	flock2 := flock.NewFlock(t.path)
+	locked, err = flock2.TryRLock()
+	c.Assert(err, IsNil)
+	c.Check(locked, Equals, true)
+
+	// make sure we just return false with no error in cases
+	// where we would have been blocked
+	t.flock.Unlock()
+	flock2.Unlock()
+	t.flock.Lock()
+	locked, err = flock.NewFlock(t.path).TryRLock()
 	c.Assert(err, IsNil)
 	c.Check(locked, Equals, false)
 }
@@ -113,6 +155,28 @@ func (t *TestSuite) TestFlock_TryLockContext(c *C) {
 	c.Check(locked, Equals, false)
 }
 
+func (t *TestSuite) TestFlock_TryRLockContext(c *C) {
+	// happy path
+	ctx, cancel := context.WithCancel(context.Background())
+	locked, err := t.flock.TryRLockContext(ctx, time.Second)
+	c.Assert(err, IsNil)
+	c.Check(locked, Equals, true)
+
+	// context already canceled
+	cancel()
+	locked, err = flock.NewFlock(t.path).TryRLockContext(ctx, time.Second)
+	c.Assert(err, Equals, context.Canceled)
+	c.Check(locked, Equals, false)
+
+	// timeout
+	t.flock.Lock()
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+	locked, err = flock.NewFlock(t.path).TryRLockContext(ctx, time.Second)
+	c.Assert(err, Equals, context.DeadlineExceeded)
+	c.Check(locked, Equals, false)
+}
+
 func (t *TestSuite) TestFlock_Unlock(c *C) {
 	var err error
 
@@ -124,6 +188,7 @@ func (t *TestSuite) TestFlock_Unlock(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(locked, Equals, true)
 	c.Assert(t.flock.Locked(), Equals, true)
+	c.Check(t.flock.RLocked(), Equals, false)
 
 	_, err = os.Stat(t.path)
 	c.Assert(os.IsNotExist(err), Equals, false)
@@ -131,16 +196,19 @@ func (t *TestSuite) TestFlock_Unlock(c *C) {
 	err = t.flock.Unlock()
 	c.Assert(err, IsNil)
 	c.Check(t.flock.Locked(), Equals, false)
+	c.Check(t.flock.RLocked(), Equals, false)
 }
 
 func (t *TestSuite) TestFlock_Lock(c *C) {
 	c.Assert(t.flock.Locked(), Equals, false)
+	c.Check(t.flock.RLocked(), Equals, false)
 
 	var err error
 
 	err = t.flock.Lock()
 	c.Assert(err, IsNil)
 	c.Check(t.flock.Locked(), Equals, true)
+	c.Check(t.flock.RLocked(), Equals, false)
 
 	// test that the short-circuit works
 	err = t.flock.Lock()
@@ -170,5 +238,51 @@ func (t *TestSuite) TestFlock_Lock(c *C) {
 	c.Assert(ok, Equals, true)
 	c.Assert(errCh, IsNil)
 	c.Check(t.flock.Locked(), Equals, false)
+	c.Check(t.flock.RLocked(), Equals, false)
 	c.Check(gf.Locked(), Equals, true)
+	c.Check(gf.RLocked(), Equals, false)
+}
+
+func (t *TestSuite) TestFlock_RLock(c *C) {
+	c.Assert(t.flock.Locked(), Equals, false)
+	c.Check(t.flock.RLocked(), Equals, false)
+
+	var err error
+
+	err = t.flock.RLock()
+	c.Assert(err, IsNil)
+	c.Check(t.flock.Locked(), Equals, false)
+	c.Check(t.flock.RLocked(), Equals, true)
+
+	// test that the short-circuit works
+	err = t.flock.RLock()
+	c.Assert(err, IsNil)
+
+	//
+	// Test that RLock() is a blocking call
+	//
+	ch := make(chan error, 2)
+	gf := flock.NewFlock(t.path)
+	defer gf.Unlock()
+
+	go func(ch chan<- error) {
+		ch <- nil
+		ch <- gf.RLock()
+		close(ch)
+	}(ch)
+
+	errCh, ok := <-ch
+	c.Assert(ok, Equals, true)
+	c.Assert(errCh, IsNil)
+
+	err = t.flock.Unlock()
+	c.Assert(err, IsNil)
+
+	errCh, ok = <-ch
+	c.Assert(ok, Equals, true)
+	c.Assert(errCh, IsNil)
+	c.Check(t.flock.Locked(), Equals, false)
+	c.Check(t.flock.RLocked(), Equals, false)
+	c.Check(gf.Locked(), Equals, false)
+	c.Check(gf.RLocked(), Equals, true)
 }
