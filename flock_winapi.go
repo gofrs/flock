ffd5ea4379c04ef93e1e250a3cf689d4af99bd19
David E. Wheeler <david.wheeler@iovation.com>
2018-01-22 15:57:44 -0500

    Add support for shared locks
    Done by adding *RLock* variants of the Lock* methods and removing the file
    truncation. The two sets of methods rely the same code by dispatching to
    shared methods, only varying the lock type. The exception is RLocked(), which
    checks its own, separate boolean. All the lock operations share the same
    mutex, though, so any all operations can block each other. The docs have been
    updated to reflet that fact. Also fixed the Windows Unlock() docs to refer to
    UnlockFileEx() instead of syscall.LOCK_UN.

--- flock_winapi.go
+++ flock_winapi.go
@@ -20,7 +20,16 @@ var (
 const (
 	winLockfileFailImmediately = 0x00000001
 	winLockfileExclusiveLock   = 0x00000002
+	winLockfileSharedLock      = 0x00000000
 )
+
+// Use of 0x00000000 for the shared lock is a guess based on some the MS Windows
+// `LockFileEX` docs, which document the `LOCKFILE_EXCLUSIVE_LOCK` flag as:
+//
+// > The function requests an exclusive lock. Otherwise, it requests a shared
+// > lock.
+//
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa365203(v=vs.85).aspx
 
 func lockFileEx(handle syscall.Handle, flags uint32, reserved uint32, numberOfBytesToLockLow uint32, numberOfBytesToLockHigh uint32, offset *syscall.Overlapped) (bool, syscall.Errno) {
 	r1, _, errNo := syscall.Syscall6(
