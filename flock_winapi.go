467b5679cf2e642c79b9f0f68432c1f474d4930c
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-06-26 17:26:46 +0200

    fix: replace Syscall6 with SyscallN on Windows

--- flock_winapi.go
+++ flock_winapi.go
@@ -32,7 +32,7 @@ const (
 // https://msdn.microsoft.com/en-us/library/windows/desktop/aa365203(v=vs.85).aspx
 
 func lockFileEx(handle syscall.Handle, flags uint32, reserved uint32, numberOfBytesToLockLow uint32, numberOfBytesToLockHigh uint32, offset *syscall.Overlapped) (bool, syscall.Errno) {
-	r1, _, errNo := syscall.Syscall6(
+	r1, _, errNo := syscall.SyscallN(
 		uintptr(procLockFileEx),
 		6,
 		uintptr(handle),
