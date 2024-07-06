396584b947c52eaca960ed66258085fe7e3a16bd
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-07-06 18:36:08 +0200

    refactor: reuse setFh

--- flock_unix.go
+++ flock_unix.go
@@ -9,7 +9,6 @@ package flock
 
 import (
 	"errors"
-	"os"
 
 	"golang.org/x/sys/unix"
 )
@@ -200,13 +199,11 @@ func (f *Flock) reopenFDOnError(err error) (bool, error) {
 	_ = f.fh.Close()
 	f.fh = nil
 
-	// reopen in read-write mode and set the file handle
-	fh, err := os.OpenFile(f.path, f.flag, f.perm)
+	// reopen the file handle
+	err = f.setFh()
 	if err != nil {
 		return false, err
 	}
-
-	f.fh = fh
 
 	return true, nil
 }
